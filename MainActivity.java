3c2b9a14b5a04fb27cf1bacff00b271469c2b8c0
Valery Shmelev <114564020+vallshmeleff@users.noreply.github.com>
2022-10-20 13:32:49 +0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -9,13 +9,22 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-// MINIMAL SQLite database JAVA Code
+import android.widget.TextView;
+
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
     Button buttonAdd;
     Button buttonRead;
     Button buttonClear;
+    Button buttonPrev;
+    Button buttonNext;
     EditText evName;
     EditText evEmail;
+    long rowID;
+    int ie=0; // First record pointer
+    int ecounter; // How many records are in the database
+
     DBHelper dbHelper;
 
     @Override
@@ -32,10 +41,16 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         buttonClear = (Button) findViewById(R.id.btnClear);
         buttonClear.setOnClickListener((View.OnClickListener) this);
 
+        buttonPrev = (Button) findViewById(R.id.btnPrev);
+        buttonPrev.setOnClickListener((View.OnClickListener) this);
+
+        buttonNext = (Button) findViewById(R.id.btnNext);
+        buttonNext.setOnClickListener((View.OnClickListener) this);
+
         evName = (EditText) findViewById(R.id.etName);
         evEmail = (EditText) findViewById(R.id.etEmail);
 
-        dbHelper = new DBHelper(this); // Create object for creating and managing database versions
+        dbHelper = new DBHelper(this); // Create an object for creating and managing database versions
     }
 
     @Override
@@ -46,47 +61,87 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         SQLiteDatabase database = dbHelper.getWritableDatabase(); // Connecting to the Database
         ContentValues contentValues = new ContentValues(); // Create an object for the data
 
-
         switch (v.getId()) {
 
             case R.id.btnAdd:
                 contentValues.put(DBHelper.KEY_NAME, name); // Adds a new row KEY_NAME to the table
                 contentValues.put(DBHelper.KEY_MAIL, email); // Adds a new row KEY_MAIL to the table
 
-                long rowID = database.insert(DBHelper.TABLE_CONTACTS, null, contentValues); // Write to the database and get its ID
+                rowID = database.insert(DBHelper.TABLE_CONTACTS, null, contentValues); // Write to the database and get its ID
                 Log.d("SQLite","== == Row inserted, ID = " + rowID);
                 break;
 
-            case R.id.btnRead:
+            case R.id.btnRead: // Set Recor 0
                 // We make a request for all data from the TABLE_CONTACTS table, we get the cursor
                 Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
-                int ecounter = database.rawQuery("SELECT _ID FROM contacts", null).getCount();
+                ecounter = database.rawQuery("SELECT _ID FROM NTable", null).getCount();
                 Log.d("== == SQLite","Count rows = " + String.valueOf(ecounter));
+                TextView evCounter = (TextView)findViewById(R.id.Rowcount);
+                evCounter.setText("Total records: " + String.valueOf(ecounter) + " Row ID:  " + Objects.toString(rowID, null));
 
-                if (cursor.moveToFirst()) { // Set the cursor position to the first line of the selection
-                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
-                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
-                    int emailIndex = cursor.getColumnIndex(DBHelper.KEY_MAIL);
-                    do {
-                        Log.d("mLog", "== Record ID = " + cursor.getInt(idIndex) +
-                                ", Name = " + cursor.getString(nameIndex) +
-                                ", E-mail = " + cursor.getString(emailIndex));
-                    } while (cursor.moveToNext());
-                } else
-                    Log.d("mLog","0 rows");
+                int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+                int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
+                int emailIndex = cursor.getColumnIndex(DBHelper.KEY_MAIL);
 
-                cursor.close(); // Free memory
+                ie =0;
+                cursor.moveToPosition(ie); // Go to first record Record 0
+                Log.d("mLog", "== == SQLite == == " + " ie: " + String.valueOf(ie) + " ID: " + cursor.getInt(idIndex) +
+                        ", Name = " + cursor.getString(nameIndex) +
+                        ", E-mail = " + cursor.getString(emailIndex));
+
+                cursor.close();
                 break;
 
             case R.id.btnClear: // Delete all entries
                 database.delete(DBHelper.TABLE_CONTACTS, null, null);
                 break;
+
+            case R.id.btnNext: // Next record
+                Cursor cursorN = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
+                ecounter = database.rawQuery("SELECT _ID FROM NTable", null).getCount();
+                idIndex = cursorN.getColumnIndex(DBHelper.KEY_ID);
+                nameIndex = cursorN.getColumnIndex(DBHelper.KEY_NAME);
+                emailIndex = cursorN.getColumnIndex(DBHelper.KEY_MAIL);
+
+                ie++;
+                if (ie > ecounter-1) {
+                    ie = 0;
+                }
+                cursorN.moveToPosition(ie); // Go to post
+
+                Log.d("SQLite","====== Record =======" + " ie: " + String.valueOf(ie));
+                cursorN.moveToPosition(ie); // Перейти к записи
+                Log.d("mLog", "== == SQLite == == " + " ie: " + String.valueOf(ie) + " ID: " + cursorN.getInt(idIndex) +
+                        ", Name = " + cursorN.getString(nameIndex) +
+                        ", E-mail = " + cursorN.getString(emailIndex));
+
+                cursorN.close();
+                break;
+
+            case R.id.btnPrev: // Previous record
+                Cursor cursorP = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
+                ecounter = database.rawQuery("SELECT _ID FROM NTable", null).getCount();
+                idIndex = cursorP.getColumnIndex(DBHelper.KEY_ID);
+                nameIndex = cursorP.getColumnIndex(DBHelper.KEY_NAME);
+                emailIndex = cursorP.getColumnIndex(DBHelper.KEY_MAIL);
+
+                ie--;
+                if (ie <0 ) {
+                    ie = ecounter-1;
+                }
+                cursorP.moveToPosition(ie); // Go to post
+
+                Log.d("SQLite","====== Record 2 =======" + " ie: " + String.valueOf(ie));
+                cursorP.moveToPosition(ie); // Перейти к записи
+                Log.d("mLog", "== == SQLite == == " + " ie: " + String.valueOf(ie) + " ID: " + cursorP.getInt(idIndex) +
+                        ", Name = " + cursorP.getString(nameIndex) +
+                        ", E-mail = " + cursorP.getString(emailIndex));
+
+                cursorP.close();
+                break;
         }
+
         dbHelper.close();
     }
-
-
-
-
 
 }
